454ff6820a7524bca642276a9dfbeaae7366f767
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-10-27 12:27:12 +0600

    api created

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
+const cors = require('cors')
 app.use(cors())
 
 const port = process.env.PORT || 5000;
 
+const subjects = require("./Data/subjects.json")
+
 app.get('/', (req, res) => {
     res.send("Surver running")
 })
+
+app.get('/subjects', (req, res) => {
+    res.send(subjects)
+})
+
 
 app.listen(port, () => {
     console.log(`serving on port: ${port}`)
